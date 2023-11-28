145b6a3280e6b180cbb3fec12934b70da936f571
Lucas Paiva <lucashtpaiva@gmail.com>
2023-11-28 09:01:44 -0300

    subindo novas correções

--- components/UserDetails.tsx
+++ components/UserDetails.tsx
@@ -1,5 +1,5 @@
+import { useFetch } from "@/pages/api/useFetch";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 type User = {
   id: string;
@@ -8,18 +8,25 @@ type User = {
 
 export default function UserDetails() {
   const router = useRouter();
+  const { id } = router.query;
 
-  const [data, setData] = useState<User>();
+  // const [data, setData] = useState<User>();
 
-  useEffect(() => {
-    if (!router.isReady) return;
-    const { id } = router.query;
-    fetch(`http://localhost:3333/users/${id}`).then((response) => {
-      response.json().then((user) => {
-        setData(user);
-      });
-    });
-  }, [router.isReady]);
+  // useEffect(() => {
+  //   if (!router.isReady) return;
+  //   const { id } = router.query;
+  //   fetch(`http://localhost:3333/users/${id}`).then((response) => {
+  //     response.json().then((user) => {
+  //       setData(user);
+  //     });
+  //   });
+  // }, [router.isReady]);
+
+  const { data } = useFetch<User>(`http://localhost:3333/users/${id}`);
+
+  if (!data) {
+    return <p>carregando...</p>;
+  }
 
   return (
     <ul>
--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@chakra-ui/react";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 type User = {
@@ -7,18 +8,35 @@ type User = {
 };
 
 export default function UserList() {
+  // const [data, setData] = useState<User[]>();
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/users").then((response) => {
+  //     response.json().then((users) => {
+  //       setData(users);
+  //     });
+  //   });
+  // }, []);
+
   const [data, setData] = useState<User[]>();
+
   useEffect(() => {
-    fetch("http://localhost:3333/users").then((response) => {
-      response.json().then((users) => {
-        setData(users);
-      });
-    });
+    axios
+      .get("http://localhost:3333/users")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => console.log(error));
   }, []);
+
+  // const { data } = useFetch<User[]>("http://localhost:3333/users");
+
+  // if (!data) {
+  //   return <p>carregando...</p>;
+  // }
 
   return (
     <ul>
-      {data?.map((user) => (
+      {data?.map((user: any) => (
         <li key={user.id}>
           <Link href={`/users/${user.id}`}>{user.name}</Link>
         </li>
